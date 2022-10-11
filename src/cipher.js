1cf80e122909280fc3eb10c47d0ae46364e08b8c
Silvana Gonzalez <algomio2000@gmail.com>
2022-10-11 11:16:23 -0500

    Codigo final

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,50 @@
-var alfabeto= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
-
 const cipher = {
-  // ...
+
+    encode: function(texto, correr) {
+      //array de letras
+      let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      //reultado para cambiar mas adelante
+      let resultado = "";
+      //operacion matematica 
+      correr = (correr % 26 + 26) % 26;
+      //condicional donde i representa la posicion del primer elemento del array y lengh nos da lo largo del mensaje, y cada vez se añade 1
+      if (texto) {
+        //inicia en 0, que la i es menor que la logitud del texto, se actualiza agragndo 1
+        for (let i = 0; i < texto.length; i++) {
+    
+          //n
+          if (letras.indexOf(texto[i]) != -1) {
+            //Pasamos el array por indexof para que nos retorne la primera poscion del texto le sumamos  el valor de correr
+            //y le aplicamos el modulo para asegurarnos que si excede los 26 devuelva un numero dentro de los valores.
+            let posicion = ((letras.indexOf(texto[i]) + correr) % 26);
+            resultado = resultado + letras[posicion];
+          }
+          else {
+            resultado += texto[i];
+          }
+        }
+      }
+      return resultado;
+    },
+    
+    decode: function(texto, correr) {
+      let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let resultado = "";
+      correr = (correr % 26 - 26) % 26;
+      if (texto) {
+        for (i = 0; i < texto.length; i++) {
+          if (letras.indexOf(texto[i]) != -1) {
+            let posicion = ((letras.indexOf(texto[i]) - correr) % 26);
+            resultado += letras[posicion];
+          }
+          else
+            resultado = resultado + letras[posicion];
+        }
+      }
+      return resultado;
+    }
 };
+// encode(correr,texto)
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import cipher from './cipher.js';
 
+//haciendo que lo que se escriba en mensaje 1 sea mayuscula
+document.getElementById("mensaje1").addEventListener("keyup",function(){
+    this.value = this.value.toUpperCase()
+  });
+  //los elementos nesesarios para la funcion encode
+  document.getElementById("encode").addEventListener("click",function(){
+    let texto = document.getElementById ("mensaje1").value;
+    let correr = document.getElementById("correr").value;
+    document.getElementById("mensaje2").value = cipher.encode(texto,correr);
+  })
+  //los elementos nesesarios para la funcion decode
+  document.getElementById("decode").addEventListener("click",function(){
+    let texto= document.getElementById ("mensaje1").value;
+    let correr = document.getElementById ("correr").value;
+    document.getElementById ("mensaje2").value = cipher.decode(texto,correr);
+  })
+
+  
+
+
+
+
